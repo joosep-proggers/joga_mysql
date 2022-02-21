b6651da8a4008815563528745e15a00d49bbd7b6
Joosep Elias Tõru <joosep-elias.toru@khk.ee>
2022-02-21 11:23:29 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const express = require('express')
 const app = express()
 
 const path = require('path')
+
 // add template engine
 const hbs = require('express-handlebars');
+
 // setup template engine directory and file extensions
 app.set('views', path.join(__dirname, 'views'));
-app.set('view-engine', 'hbs');
+app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
 	extname: 'hbs',
 	defaultLayout: 'main',
-	layoutsDir: __dirname + '/views/layouts',
+	layoutsDir: __dirname + '/views/layouts/',
 }))
+
 // setup static public directory
 app.use(express.static('public'));
 
@@ -34,6 +37,18 @@ con.connect(function(err){
 	console.log("Connected to joga_mysql database")
 })
 
+// show all articles - index page
+app.get('/', (req, res) => {
+	let query = "SELECT * FROM article";
+	let articles = []
+	con.query(query, (err, result) => {
+		if (err) throw err;
+		articles = result
+		res.render('index', {
+			articles: articles
+		})
+	})
+});
 // app start point
 app.listen(3000, () => {
 	console.log('App is started at http://localhost:3000')
