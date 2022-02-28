1869d2a6c8474717002e557b7487777a4be1dfc6
Joosep Elias Tõru <joosep-elias.toru@khk.ee>
2022-02-28 21:09:43 +0200

    Created article controller and route, refactored application code

--- index.js
+++ index.js
@@ -19,49 +19,15 @@ app.engine('hbs', hbs.engine({
 // setup static public directory
 app.use(express.static('public'));
 
-const mysql = require('mysql')
-
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 
-// create database connection
-var con = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "qwerty",
-	database: "joga_mysql"
-})
 
-con.connect(function(err){
-	if (err) throw err;
-	console.log("Connected to joga_mysql database")
-})
+const articleRoutes = require('./routes/article'); // import article route
 
-// show all articles - index page
-app.get('/', (req, res) => {
-	let query = "SELECT * FROM article";
-	let articles = []
-	con.query(query, (err, result) => {
-		if (err) throw err;
-		articles = result
-		res.render('index', {
-			articles: articles
-		})
-	})
-});
-
-// show article by this slug
-app.get('/article/:slug', (req, res) => {
-	let query = `SELECT article.*, author.name AS "nameAuthor" FROM article INNER JOIN author ON article.author_id = author.id WHERE slug="${req.params.slug}"`
-	let article
-	con.query(query, (err, result) => {
-		if (err) throw err;
-		article = result;
-		res.render('article', {
-			article: article
-		})
-	})
-})
+// to use article routes
+app.use('/', articleRoutes);
+app.use('/article', articleRoutes)
 
 // show author articles
 app.get('/author/:id', (req,res) => {
